da20ae139977d6103e23f6fce02caacf949df7ed
tonmoystark <tonmoy.a009@gmail.com>
2026-04-12 19:32:11 +0600

    nothing special.

--- day3/github.js
+++ day3/github.js
@@ -1,38 +1,34 @@
-let container = document.getElementById('container');
-
+let container = document.getElementById("container");
 async function github() {
-    container.textContent = 'fetching data'
-    try {
-        let response = await fetch('https://jsonplaceholder.typicode.com/users')
-        let data = await response.json()
-        container.textContent = ''
-        console.log(data);
-        
-        data.forEach(element => { 
-            let subContainer = document.createElement('div');
-            let username = document.createElement('h1')
-            let email = document.createElement('h3')
-            let company = document.createElement('h3')
-            let phone = document.createElement('h4')
-            let website = document.createElement('h4')
-            username.textContent = element.name
-            email.textContent = element.email
-            company.textContent = element.company.name
-            phone.textContent = element.phone
-            website.textContent = element.website
-            subContainer.append(username)
-            subContainer.append(email)
-            subContainer.append(company)
-            subContainer.append(phone)
-            subContainer.append(website)
-            container.append(subContainer);
+  container.textContent = "fetching data";
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    container.textContent = "";
+    console.log(data);
 
-        });
-
-    } catch (error) {
-        container.textContent = error.message
-    }
+    data.forEach((element) => {
+      let subContainer = document.createElement("div");
+      let username = document.createElement("h1");
+      let email = document.createElement("h3");
+      let company = document.createElement("h3");
+      let phone = document.createElement("h4");
+      let website = document.createElement("h4");
+      username.textContent = element.name;
+      email.textContent = element.email;
+      company.textContent = element.company.name;
+      phone.textContent = element.phone;
+      website.textContent = element.website;
+      subContainer.append(username);
+      subContainer.append(email);
+      subContainer.append(company);
+      subContainer.append(phone);
+      subContainer.append(website);
+      container.append(subContainer);
+    });
+  } catch (error) {
+    container.textContent = error.message;
+  }
 }
 
-
-github()
+github();
