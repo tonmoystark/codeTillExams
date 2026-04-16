d22d85d2ed5e354fbfadbdddb0442dcfb8cd5851
tonmoystark <tonmoy.a009@gmail.com>
2026-04-16 18:24:35 +0600

    Enterted into react

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Name from './components/day8/Name'
+
+const App = () => {
+  return (
+    <div className='bg-slate-700 text-white h-screen'>
+      <Name />
+    </div>
+  )
+}
+
+export default App
